5e19125ce8b7f5c5b343e0e57e6e576261adf6fe
iranika <7370486+iranika@users.noreply.github.com>
2019-05-19 01:34:24 +0900

    fix: viewer.jsの廃止

--- mo-code/mocode.js
+++ mo-code/mocode.js
@@ -1,75 +1,80 @@
-const elem = {
-  view : document.getElementById("view"),
-  more : document.getElementById("more"),
-  footer:document.getElementById("footer"),
-}
-var page = {
-  current : 0,
-  length : pageData.length -1,
-}
-var conf = {
-  isAutoShowMore : true
-}
-
-window.onload = () => {
-  //document.getElementById("view").innerHTML = ""
-  initShowImage(page.current) //モーダルに最初から読むを実装したため、初期化処理からは除外
-
-  viewer.addTitleToSideBar("rightMenu")
-  
-  //続きを表示が押されたときの処理
-  elem.more.onclick = ()=>{
-    showMore()
+//viewerの定義
+const viewer = {
+  elem : {
+    view : document.getElementById("view"),
+    more : document.getElementById("more"),
+    footer : document.getElementById("footer"),
+    rightMenu : document.getElementById("rightMenu"),
+  },
+  conf : {
+    isAutoShowMore : true
+  },
+  pageData : pageData,
+  page : {
+    current : 0,
+    length : pageData.length -1,
+  },
+  openRightMenu : (element=viewer.elem.rightMenu)=>{
+    element.style.display = "block";
+  },
+  closeRightMenu : (element=viewer.elem.rightMenu)=>{
+    element.style.display = "none";
+  },
+  addTitleToSideBar : (element=viewer.elem.rightMenu)=>{
+    viewer.pageData.forEach((item, index) => {
+      element.innerHTML += viewer.sidevarItem(index, item)
+    })
+  },
+  sidevarItem : (index, item)=>{
+    return `<a href="#" class="w3-bar-item w3-button" onclick="viewer.initShowImage(${index})">${index}. ${item.Title}</a>`
+  },
+  showMore : ()=>{
+    if (viewer.page.length >= viewer.page.current){
+      viewer.showHiddenImage()
+      viewer.addHiddenImage(viewer.page.current + 1)
+      viewer.page.current += 1  
+    }
+  },
+  initShowImage : (pageNum)=>{
+    viewer.elem.view.innerHTML = ""
+    viewer.addHiddenImage(pageNum)
+    viewer.showHiddenImage()
+    viewer.addHiddenImage(pageNum + 1)
+    viewer.page.current = pageNum + 1
+    viewer.closeRightMenu()
+  },
+  lastShowImage: ()=>{
+    viewer.initShowImage(viewer.page.length)
+  },
+  showHiddenImage : ()=>{
+    var elems = document.querySelectorAll(".hidden-image")
+    if (elems.length != 0){
+      [].forEach.call(elems,(elem) => {
+        elem.className = elem.className.replace("hidden-image", "")
+      });
+    }
+  },
+  addHiddenImage : (pageNum)=>{
+    if (pageNum > viewer.page.length){
+      return
+    }
+    viewer.pageData[pageNum].ImagesUrl.forEach(img_url => {
+      viewer.elem.view.innerHTML += `<img class="hidden-image" src="${img_url}">`
+    })
   }
+}
 
+window.onload = ()=>{
+  viewer.initShowImage(viewer.page.current)
+  viewer.addTitleToSideBar(viewer.elem.rightMenu)
   var triggerMargin = 5
   //sub funcions
   window.addEventListener("scroll", function(){
     //console.log(elem.footer.getBoundingClientRect().bottom)
-    if (!conf.isAutoShowMore){ return }
-    if (triggerMargin > elem.footer.getBoundingClientRect().bottom - window.innerHeight){
-      showMore()      
+    if (!viewer.conf.isAutoShowMore){ return }
+    if (triggerMargin > viewer.elem.footer.getBoundingClientRect().bottom - window.innerHeight){
+      viewer.showMore()  
     }
-  })
-}
-
-function showMore(){
-  if (page.length >= page.current){
-    showHiddenImage()
-    addHiddenImage(page.current + 1)
-    page.current += 1  
-  }
-}
-
-function initShowImage(pageNum){
-  elem.view.innerHTML = ""
-  addHiddenImage(pageNum)
-  showHiddenImage()
-  addHiddenImage(pageNum + 1)
-  page.current = pageNum + 1
-  viewer.closeRightMenu()
-}
-
-function lastShowImage(){
-  initShowImage(page.length)
-}
-
-function showHiddenImage(){
-  var elems = document.querySelectorAll(".hidden-image")
-  if (elems.length != 0){
-    [].forEach.call(elems,(elem) => {
-      elem.className = elem.className.replace("hidden-image", "")
-    });
-  }
-}
-
-function addHiddenImage(pageNum){
-  if (pageNum > page.length){
-    return
-  }
-  var pg = pageData[pageNum]
-  pg.ImagesUrl.forEach(img_url => {
-    elem.view.innerHTML += `<img class="hidden-image" src="${img_url}">`
   })
 }
 
